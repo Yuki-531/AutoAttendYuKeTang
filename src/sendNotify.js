0e4dac8f4977c4d4f16d5688661f83de4ef78485
Yuki-531 <54757984+Yuki-531@users.noreply.github.com>
2022-04-27 00:26:31 +0800

    Update sendNotify.js

--- src/sendNotify.js
+++ src/sendNotify.js
@@ -23,7 +23,7 @@ const sendNotify = (title, message) => {
   }
 
   if (SCKEY) {
-    got(`https://sctapi.ftqq.com/${SCKEY}.send`, {
+    got(`http://pushplus.hxtrip.com/${SCKEY}.send`, {
       searchParams: {
         text: title,
         desp: message,
@@ -32,11 +32,11 @@ const sendNotify = (title, message) => {
     })
       .then(({ body }) => {
         if (body.code === 0) {
-          console.log('server酱发送通知消息成功\n')
+          console.log('发送通知消息成功\n')
         } else if (body.code === 1024) {
-          console.log(`server酱发送通知消息异常: ${body.errmsg}\n`)
+          console.log(`发送通知消息异常: ${body.errmsg}\n`)
         } else {
-          console.log(`server酱发送通知消息异常\n${JSON.stringify(body)}`)
+          console.log(`发送通知消息异常\n${JSON.stringify(body)}`)
         }
       })
       .catch((err) => {
@@ -44,7 +44,7 @@ const sendNotify = (title, message) => {
         console.log(err)
       })
   } else {
-    console.log('您未提供server酱的SCKEY，取消微信推送消息通知\n')
+    console.log('您未提供SCKEY，取消微信推送消息通知\n')
   }
 }
 
